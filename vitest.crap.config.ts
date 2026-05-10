a161bb9472f46a02eb23f1b54847540f908ae9a0
Fabian Barney <fabian.barney@barney-media.com>
2026-05-10 21:29:46 +0200

    Bump @barney-media/crap-typescript-vitest to 0.3.0

--- vitest.crap.config.ts
+++ vitest.crap.config.ts
@@ -4,6 +4,9 @@ import baseConfig from "./vitest.config";
 
 export default withCrapTypescriptVitest(baseConfig, {
   changedOnly: false,
+  failuresOnly: true,
+  format: "text",
+  junit: false,
   packageManager: "npm",
   paths: ["src"],
   projectRoot: process.cwd()
